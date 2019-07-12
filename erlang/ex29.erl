e5e6cc49a537155fb58f28255e8edeae1f1925a6
Syed Awase Khirni <awasekhirni@gmail.com>
2019-07-12 19:22:19 +0530

    Add files via upload

--- erlang/ex29.erl
+++ erlang/ex29.erl
@@ -2,3 +2,77 @@
 %% http://www.territorialprescience.com or www.sycliq.com
 %% awasekhirni@gmail.com
 %% Penalties & Liabilities apply copying the code without seeking permission
+
+%% Records
+%%A record in erlang is similar to a struct in C.
+%Used to store a fixed number of elements. 
+%Record expressions are translated to tuple expressions during compilation.
+%To define a record use the “-record” directive
+%Fields can have default values. 
+
+%%
+%%
+
+-module(ex29).
+-compile(export_all).
+
+%record definition of "species"
+-record(species,{
+    name,
+    color,
+    life=1,
+    attack=0,
+    abilities=[]
+}).
+
+% create an instance of the reord
+animal()->#species{
+    name="panther",
+    color="black ",
+    attack=1,
+    abilities=[hunt,climb,jump]
+}.
+
+
+%record definition 
+-record(celldata,{
+    state, %State=0|1
+    nextstate, %State=0|1
+    neighbors, %[Pid...]
+    neighborstates %[{Pid,State},..]
+}).
+
+% record definition
+-record(employee,{empid,empname,emprole,empsalary}).
+
+
+% nested record definition 
+-record(person,{name,age,gender,height,weight}).
+-record(staff,{person,staffid}).
+
+-record(player,{playerName,cards,energy}).
+-record(board,{city_center,outside}).
+-record(gamestate,{board,player,dices,game_round}).
+
+%%add_player(Name, #gamestate{ board = Board,player = #player{playerName = PlayerName, cards = Cards, energy = Energy},dices = Dices,game_round = Game_round}) ->
+
+
+run()->
+    Syed=#employee{empid=123123,empname="Syed Awase",emprole="CEO",empsalary=12383243},
+    io:fwrite("~p~n",[Syed#employee.empid]),
+    io:fwrite("~p~n",[Syed#employee.empname]),
+    io:fwrite("~p~n",[Syed#employee.emprole]),
+    io:fwrite("~p~n",[Syed#employee.empsalary]),
+    Aicy=Syed#employee{empname="Syed Awase Khirni"},
+    io:fwrite("~p~n",[Aicy#employee.empname]),
+    %% instantiating nested record definitions
+    StaffEngineer=#staff{person=#person{name="rumi",age=23,gender="male",height=174,weight=79},staffid=872137},
+    io:fwrite("~p~n",[StaffEngineer#staff.staffid]),
+    io:fwrite("~p~n",[StaffEngineer#staff.person]),
+    io:fwrite("~p~n",[StaffEngineer#staff.person#person.name]),
+    %% fetching all the fields of the record
+    io:format("Record fields:~p~n",[record_info(fields,staff)]),
+    %% fetching the size of the record
+    io:format("Record Size: ~p~n",[record_info(size,staff)]).
+
+
--- erlang/ex30.erl
+++ erlang/ex30.erl
@@ -2,3 +2,36 @@
 %% http://www.territorialprescience.com or www.sycliq.com
 %% awasekhirni@gmail.com
 %% Penalties & Liabilities apply copying the code without seeking permission
+
+%% strings in erlang
+
+%% string operations 
+
+-module(ex30).
+-import(string,[len/1,equal/2,chr/2,concat/2,substr/3,to_upper/1]).
+-compile(export_all).
+
+
+start()->
+    MyString="You Know Hello, you fool i love you,come on enjoy the joy ride!",
+    io:fwrite("~p~n",[MyString]),
+    %% compute the length of the string
+    Leny=len(MyString),
+    io:fwrite("~p~n",[Leny]),
+    %% compare two strings if they are equal 
+    MyString2=("Hakuna Matata What a wonderful world"),
+    Status=equal(MyString,MyString2),
+    io:fwrite("~p~n",[Status]),
+    %% convert string to upper case or lower case 
+    %% to_lower or to_upper
+    MyString3=to_upper(MyString2),
+    io:fwrite("~p~n",[MyString3]),
+    %% concatenate two strings 
+    MyString4=concat(MyString,MyString2),
+    io:fwrite("~p~n",[MyString4]),
+    %% fetch the index position of the character in the string 
+    MyCharIndex=chr(MyString2,$n),
+    io:fwrite("~p~n",[MyCharIndex]),
+    %% fetch the substr
+    MySubs=substr(MyString2,8,19),
+    io:fwrite("~p~n",[MySubs]).
